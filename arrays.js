fe0621a80f46d4e2c7118ea01cada5cc7787adce
Sadman <2258cse00945@manarat.ac.bd>
2026-07-28 13:38:25 +0600

    Add JavaScript files for arithmetic, arrays, and assignment operations examples

---
+++ arrays.js
@@ -0,0 +1,11 @@
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers);
+
+console.log(numbers[0]);
+console.log(numbers[2]);
+console.log(numbers[4]);
+
+numbers[1] = 100;
+
+console.log(numbers);
