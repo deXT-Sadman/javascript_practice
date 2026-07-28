595a7abafa6573473815d6bded2dd16181bd171c
Sadman <2258cse00945@manarat.ac.bd>
2026-07-28 13:30:33 +0600

    Add strings and typesOfOperator JavaScript files with initial examples

---
+++ strings.js
@@ -0,0 +1,10 @@
+let name = 'Sadman';
+
+console.log(name);
+
+let city = "Dhaka";
+
+console.log(city);
+
+//interpolation
+console.log(`My name is ${name} and I live in ${city}`);
---
+++ typesOfOperator.js
@@ -0,0 +1,18 @@
+console.log(typeof 100);
+console.log(typeof 5.5);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+let age = 24;
+let price = 10.5;
+
+console.log(typeof age);
+console.log(typeof price);
+
+//JavaScript has only one numeric type
+
+//Unlike Dart:
+//int age = 24;
+//double price = 10.5;
