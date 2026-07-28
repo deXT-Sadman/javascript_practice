e74d6f2caf9dde0f279338a9668783bec987bdc1
Sadman <2258cse00945@manarat.ac.bd>
2026-07-28 13:43:04 +0600

    Add JavaScript files for comparison, logical, and type conversion operations examples

---
+++ comparision_operations.js
@@ -0,0 +1,18 @@
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 >= 10);
+console.log(10 <= 8);
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 != 20);
+console.log(10 !== "10");
+
+//Coercion
+console.log(10 == "10");
+
+console.log(5 == "5");
+console.log(5 === "5");
+
+//== performs type coercion.
+//=== compares both value and type.
+//Always prefer === and !== in modern JavaScript.
---
+++ type_converstion.js
@@ -0,0 +1,6 @@
+let age = "25";
+
+let numberAge = Number(age);
+
+console.log(numberAge);
+console.log(typeof numberAge);
