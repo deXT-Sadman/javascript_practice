9bc97221894e1d82ea4f85db4fbc84b06601ac20
Sadman <2258cse00945@manarat.ac.bd>
2026-07-28 13:55:34 +0600

    Add JavaScript files for conditional statements examples (if, else if, nested if)

---
+++ module2/elseif.js
@@ -0,0 +1,11 @@
+let marks = 78;
+
+if (marks >= 80) {
+    console.log("A+");
+} else if (marks >= 70) {
+    console.log("A");
+} else if (marks >= 60) {
+    console.log("A-");
+} else {
+    console.log("Fail");
+}
