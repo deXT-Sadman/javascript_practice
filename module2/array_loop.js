c560b54dc5d4ba671683c45015108eade2a86786
Sadman <2258cse00945@manarat.ac.bd>
2026-07-31 05:34:54 +0600

    Add JavaScript files for array and nested loop examples

---
+++ module2/array_loop.js
@@ -0,0 +1,5 @@
+const languages = ['JavaScript', 'Python', 'Java', 'C++', 'Ruby'];
+
+for (let i = 0; i < languages.length; i++) {
+    console.log(`${i}: ${languages[i]}`);
+}
---
+++ module2/nested_loops.js
@@ -0,0 +1,10 @@
+for (let row = 1; row <= 3; row++) {
+
+    for (let col = 1; col <= 4; col++) {
+
+        process.stdout.write("* ");
+
+    }
+
+    console.log();
+}
