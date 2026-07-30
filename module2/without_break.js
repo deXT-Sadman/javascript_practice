03e4a18e778cda21dbd00180499ca247aabde69e
Sadman <2258cse00945@manarat.ac.bd>
2026-07-30 06:34:06 +0600

    Add JavaScript files for do-while loop, for loop, multiplication table, while loop, and switch statement examples

---
+++ module2/without_break.js
@@ -0,0 +1,16 @@
+let day = 2;
+
+switch (day) {
+
+    case 1:
+        console.log("Monday");
+
+    case 2:
+        console.log("Tuesday");
+
+    case 3:
+        console.log("Wednesday");
+
+    default:
+        console.log("Done");
+}
