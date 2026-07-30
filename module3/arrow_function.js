18f1ac241767457446c30c3b9697952c694cd06f
Sadman <2258cse00945@manarat.ac.bd>
2026-07-31 05:47:35 +0600

    Add JavaScript files for arrow functions and function expressions examples

---
+++ module3/arrow_function.js
@@ -0,0 +1,17 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(add(20, 30));
+
+const sub = (a, b) => a - b;
+
+console.log(sub(50, 20));
+
+const square = number => number * number;
+
+console.log(square(9));
+
+const hello = () => console.log("Hello");
+
+hello();
---
+++ module3/function_expression.js
@@ -0,0 +1,17 @@
+const greet = function () {
+    console.log("Hello");
+};
+
+greet();
+
+const salam = function () {
+    console.log("Assalamu Alaikum");
+}
+
+salam();
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+console.log(multiply(6, 8));
