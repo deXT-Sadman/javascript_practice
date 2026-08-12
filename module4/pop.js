d7a5056f5c1d9d45c5fa39901c5df43ddb21a2c6
Sadman <2258cse00945@manarat.ac.bd>
2026-08-12 15:41:48 +0600

    Add JavaScript files for array manipulation examples, including pop and push methods

---
+++ module4/pop.js
@@ -0,0 +1,20 @@
+const numbers = [
+    10,
+    20,
+    30
+];
+
+numbers.pop();
+
+console.log(numbers);
+
+const numbers2 = [
+    10,
+    20,
+    30
+];
+
+const removed = numbers.pop();
+
+console.log(removed);
+console.log(numbers);
---
+++ module4/push2.js
@@ -0,0 +1,22 @@
+const numbers = [
+    10,
+    20
+];
+
+numbers.push(30);
+numbers.push(40);
+numbers.push(50);
+
+console.log(numbers);
+
+const colors = [
+    "Red"
+];
+
+colors.push(
+    "Green",
+    "Blue",
+    "Yellow"
+);
+
+console.log(colors);
